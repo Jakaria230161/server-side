1cb43eedc099d45dec7bd673279e72ac54bc9a58
Jakaria230161 <jakariamahmud016@gmail.com>
2022-11-01 18:21:41 +0600

    backend work

---
+++ __step.js
@@ -0,0 +1,9 @@
+/* 
+POST
+1. Create post API
+2. Use fetch with method post
+3, add headers to the fetch: 'content-type': 'application/json'
+4, add body to the fetch to send data
+5, Make sure to use JSON.stringify to sent data
+6, use express.json() as middleware
+*/
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Simple Node Server Running');
 });
+
+app.use(cors());
+app.use(express.json());
 
 const users = [
     {id: 1, name: 'Jakaria', email: 'jakariamahmud016@gmail.com', address: 'Kushtia', University: 'Islamic University'},
@@ -15,6 +19,15 @@ const users = [
 
 app.get('/users', (req, res) => {
     res.send(users);
+})
+
+app.post('/users', (req, res) => {
+    console.log('post api called');
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    console.log(user)
+    res.send(user);
 })
 
 app.listen(port, () => {
